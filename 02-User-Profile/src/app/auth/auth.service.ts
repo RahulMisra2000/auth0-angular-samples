dab141a6042f88528dfaa7997bf3c43fe4315045
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-23 00:46:31 -0400

    Comments

--- 02-User-Profile/src/app/auth/auth.service.ts
+++ 02-User-Profile/src/app/auth/auth.service.ts
@@ -12,10 +12,10 @@ export class AuthService {
   auth0 = new auth0.WebAuth({
     clientID: AUTH_CONFIG.clientID,
     domain: AUTH_CONFIG.domain,
-    responseType: 'token id_token',
-    audience: `https://${AUTH_CONFIG.domain}/userinfo`,
+    responseType: 'token id_token',                       
+    audience: `https://${AUTH_CONFIG.domain}/userinfo`,  
     redirectUri: AUTH_CONFIG.callbackURL,
-    scope: 'openid profile'
+    scope: 'openid profile email'                         // The scopes we would like to receive in the idToken
   });
 
   userProfile: any;
@@ -38,7 +38,36 @@ export class AuthService {
       }
     });
   }
+  // ***************** authResult javascript object given to us has the following : ****************
+  // .accessToken
+  // .idToken         ____.____._____
+  // .refreshToken    null
+  // .expiresIn
+  // .idTokenPayload  (this repeats all the claims inside that are already inside the idToken)
+  // In our example because we have requested the following scopes      scope: 'openid profile email'
+  // this is what is in the .idTokenPayload
+  // This same info can also be extracted from the idToken by using some library API but the .parseHash()'s 
+  // returns the javascript object whose .idTokenPayload property has all this neatly available
+      // iss:"https://rahulmisra2000.auth0.com/"                                            // Always present
+      // iat:1529727545                                                                     // Always present
+      // exp:1529763545                                                                     // Always present
+      // sub:"auth0|5b2d5aef52e65360e5dff35b"                                               // Always present
+      // aud:"Qcq75xR4VjcLRzyUx0GjrMKDJE5dh7po"                                             // Always present
 
+      // email:"rahulmisra2000@gmail.com"                                                   because  scope: email   was specified
+      // email_verified:true                                                                because  scope: email   was specified
+      // name:"rahulmisra2000@gmail.com"                                                    because  scope: profile was specified 
+      // nickname:"rahulmisra2000"                                                          because  scope: profile was specified
+      // nonce:"UV51J7PYirEdcUzUeFzmVEbst9reC-rN"                                           because  scope: profile was specified
+      // picture:"https://s.gravatar.com/avatar/725bd805b8fffa980a
+          // 84ccf51d74a484?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fra.png"    because  scope: profile was specified
+
+  
+  
+  
+  // -----------------------------------------------------------------------------------------------------------
+  // *** By calling the .userInfo() API, we can also get hold off the stuff returned by profile and email scopes 
+  // -----------------------------------------------------------------------------------------------------------
   public getProfile(cb): void {
     const accessToken = localStorage.getItem('access_token');
     if (!accessToken) {
@@ -46,14 +75,15 @@ export class AuthService {
     }
 
     const self = this;
-    this.auth0.client.userInfo(accessToken, (err, profile) => {
+    this.auth0.client.userInfo(accessToken, (err, profile) => {               // ANOTHER LIBRARY API   .userInfo()
       if (profile) {
         self.userProfile = profile;
       }
       cb(err, profile);
     });
   }
-
+  // -----------------------------------------------------------------------------------------------------------
+  
   private setSession(authResult): void {
     // Set the time that the access token will expire at
     const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
